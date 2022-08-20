f6d5a022b62b6204b37d15ccc05ce6529519532b
VeroniqueChassan <veroniquechassan@gmail.com>
2022-08-20 16:34:19 +0400

    find the right number

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const buttons = document.querySelectorAll('.btn');
-const result = document.getElementById("result");
+//choisi un numéro entre 0 et 100
 
-buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        result.textContent += e.target.id;
-    });
-});
+let number = document.getElementById("number");
+let randomNumber = Math.floor(Math.random () * 100);
+let result = document.getElementById("result");
+let button = document.querySelector("button");
+// confetti();
 
-equal.addEventListener("click", () => {
-    result.textContent = eval(result.textContent);
-});
+//creation d'un random number
 
-clear.addEventListener("click", () => {
-    result.textContent = "";
+//onclick event sur bouton verifier
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (number.value > randomNumber) {
+        result.value = "Your number is too high, try again";
+    } else if (number.value < randomNumber) {
+        result.value = "Your number is too low, try again";
+    } else {
+        result.value = "You WON !!! CONGRATULATION !!";
+        confetti();
+    }
 })
+
+
+
+    //creation d'un random number
+    //comparaison du numéro choisir avec le random number
+        //si au-dessus => dire au-dessus
+        //si au-dessous => dire au-dessous
+        //sinon => vous avez gagné
+
